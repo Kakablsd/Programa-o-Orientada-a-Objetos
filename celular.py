cbb166e8f8d3a7bf9472f2b8417fa3da94e3f7b6
Kauê Pimentel <kauepimentel39@hotmail.com>
2025-09-24 20:37:58 -0300

    Update celular.py

--- celular.py
+++ celular.py
@@ -5,7 +5,8 @@ class Celular:
     def mostrar_modelo(modelo):
         print(f'seu modelo é: {modelo}')
 
-    def mostrar_tudo(marca, modelo):
+    def mostrar_informacoes(marca, modelo):
         print(f"seu aparelho é o {marca} {modelo}")
+
 
 
