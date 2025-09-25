79dfe4049b040bacbed90986df798d4a372ca382
Kauê Pimentel <kauepimentel39@hotmail.com>
2025-09-24 21:41:04 -0300

    Delete cachorro.py

--- cachorro.py
+++
@@ -1,9 +0,0 @@
-class Cachorro:
-    def mostrar_raca(raca):
-        print(f'A raça do cachorro é: {raca}')
-
-    def mostrar_idade(idade):
-        print(f'A idade do cachorro é: {idade}')
-
-    def mostrar_raca_idade(raca, idade):
-        print(f"seu cachorro tem {idade} anos e é da raça {raca}")
