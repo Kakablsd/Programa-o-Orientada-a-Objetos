7c107fcbbdb8093f1b579d3ef9fab670abaeab09
Kauê Pimentel <kauepimentel39@hotmail.com>
2025-09-24 21:42:02 -0300

    Add files via upload

---
+++ Atividade class sem init/cachorro.py
@@ -0,0 +1,9 @@
+class Cachorro:
+    def mostrar_raca(self,raca):
+        print(f'A raça do cachorro é: {raca}')
+
+    def mostrar_idade(self,idade):
+        print(f'A idade do cachorro é: {idade}')
+
+    def mostrar_raca_idade(self, raca, idade):
+        print(f"Seu cachorro tem {idade} anos e é da raça {raca}")
